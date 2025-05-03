144018d817aa92de59789e44c7c69bb0b12e7844
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2025-05-03 08:32:23 +0300

    Refactor product data and update Hero component styles; add Popular component for displaying items

--- sweet-pastries/src/Components/Hero/Hero.jsx
+++ sweet-pastries/src/Components/Hero/Hero.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Hero.css";
 import HeroImage from "../Assets/images/image53.jpg";
-import ArrowIcon from "../Assets/Frontend_Assets/arrow.png";
-import HandIcon from "../Assets/Frontend_Assets/hand_icon.png";
 
 const Hero = () => {
   return (
@@ -10,16 +8,12 @@ const Hero = () => {
       <div className="hero-left">
         <h2>Sweet pastries</h2>
         <div>
-          <div className="icon">
-            <p>New</p>
-            <img src={HandIcon} alt="" />
-          </div>
-          <p>Collection</p>
+          <p>Fresh</p>
+          <p>Pastries</p>
           <p>for everyone </p>
         </div>
         <div className="hero-latest-btn">
           <div>Latest cakes</div>
-          <img src={ArrowIcon} alt="" />
         </div>
       </div>
       <div className="hero-right">
