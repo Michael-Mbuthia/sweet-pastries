57274ee7159ad05b735323d0e9591e3e9cce024e
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2025-05-15 23:07:37 +0300

    Refactor CartItems component; update layout to display product details, quantities, and implement remove functionality.

--- sweet-pastries/src/Components/CartItems/CartItems.jsx
+++ sweet-pastries/src/Components/CartItems/CartItems.jsx
@@ -11,22 +11,39 @@ const CartItems = ({ items }) => {
 
   return (
     <div className="cart-items">
-      <h2>Cart</h2>
-      <ul>
-        {items.length > 0 ? (
-          items.map((item, index) => (
-            <li key={index}>
-              <img src={item.image} alt={item.name} style={{ width: "50px" }} />
-              <span>{item.name}</span> - <span>Ksh{item.price}</span>
-            </li>
-          ))
-        ) : (
-          <div>
-            <p>Your cart is empty.</p>
-            <button onClick={handleBackToShop}>Back to Shop</button>
-          </div>
-        )}
-      </ul>
+      <div className="cartitems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      {all_products.map((product) => {
+        if (cartItems[(product, id)] > 0) {
+          return (
+            <div className="cartitems-format" key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <p>{product.title}</p>
+              <p>${product.price}</p>
+              <buttton className="cartitems-quantity">
+                {cartItems[product.id]}
+              </buttton>
+              <p>Ksh{(cartItems[product.id] * product.price).toFixed(2)}</p>
+              <button onClick={() => handleRemoveFromCart(product.id)}>
+                Remove
+              </button>
+            </div>
+          );
+        }
+      })}
+      ))) : (
+      <div>
+        <p>Your cart is empty.</p>
+        <button onClick={handleBackToShop}>Back to Shop</button>
+      </div>
+      )
     </div>
   );
 };
